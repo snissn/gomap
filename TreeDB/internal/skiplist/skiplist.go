e5a7e76ea4de58c02f2c9927052a3ee1c60ef6b1
Mikers <snissn@gmail.com>
2025-12-29 20:50:47 -1000

    Revert "skiplist: reduce arena chunk to 2MiB"
    This reverts commit 25d6fe9fbeb8225652846f7d72399b9907b277a4.

--- TreeDB/internal/skiplist/skiplist.go
+++ TreeDB/internal/skiplist/skiplist.go
@@ -21,10 +21,10 @@ const (
 	flagDeleted = 1
 
 	// Arena Constants
-	// We use 2MiB chunks. With uint32 pointers, this provides a total addressable
+	// We use 4MiB chunks. With uint32 pointers, this provides a total addressable
 	// arena of 4GiB.
-	chunkShift = 21
-	chunkSize  = 1 << chunkShift // 2 MiB
+	chunkShift = 22
+	chunkSize  = 1 << chunkShift // 4 MiB
 	chunkMask  = chunkSize - 1
 )
 
