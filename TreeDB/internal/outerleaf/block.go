714211468f3f4ddad844bd06d211e37579633742
Mikers <snissn@gmail.com>
2026-02-23 16:49:58 -1000

    fix(build): define v1_leaflog_route mode constants in db + outerleaf

--- TreeDB/internal/outerleaf/block.go
+++ TreeDB/internal/outerleaf/block.go
@@ -20,6 +20,7 @@ import (
 const (
 	ModeV1LeafLog       = "v1_leaflog"
 	ModeV1LeafLogLegacy = "v1_leaflog_legacy"
+	ModeV1LeafLogRoute  = "v1_leaflog_route"
 	ModeV2BlockPtr      = "v2_blockptr"
 	ModeV2FencePtr      = "v2_fenceptr"
 
@@ -266,7 +267,7 @@ func HasMagic(payload []byte) bool {
 
 func ModeEnabled(mode string) bool {
 	switch strings.TrimSpace(mode) {
-	case ModeV1LeafLog, ModeV1LeafLogLegacy, ModeV2BlockPtr, ModeV2FencePtr:
+	case ModeV1LeafLog, ModeV1LeafLogLegacy, ModeV1LeafLogRoute, ModeV2BlockPtr, ModeV2FencePtr:
 		return true
 	default:
 		return false
