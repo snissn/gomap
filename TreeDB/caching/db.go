d686437eab8236686e0c80d2e317eb98829b63bb
Mikers <snissn@gmail.com>
2026-03-06 11:28:55 -1000

    db: separate public batch reserve and arena hints

--- TreeDB/caching/db.go
+++ TreeDB/caching/db.go
@@ -14820,7 +14820,7 @@ func (db *DB) NewBatchWithSize(size int) *Batch {
 	return &Batch{
 		db:             db,
 		entries:        db.getBatchEntries(reserveHint),
-		copyArenaCap:   db.batchCopyArenaInitCap(reserveHint),
+		copyArenaCap:   db.batchCopyArenaInitCap(size),
 		streamEligible: true,
 	}
 }
