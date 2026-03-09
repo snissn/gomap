9001c748013873dec84182df63868c25687275d5
Mikers <snissn@gmail.com>
2026-03-08 17:40:10 -1000

    caching: fix vet and gc protection test preconditions

--- TreeDB/caching/db.go
+++ TreeDB/caching/db.go
@@ -15071,17 +15071,17 @@ func (db *DB) Iterator(start, end []byte) (merging.Iterator, error) {
 		if iteratorDebugEnabled.Load() {
 			it = &debugIterator{Iterator: it, queueLen: queueLen, sourcesUsed: sourcesUsed}
 		}
-		if hasMemSource && view != nil {
-			leasedView := view
-			view = nil
-			releaseView = false
-			return &leasedMergingIterator{
-				Iterator: it,
-				release: func() {
-					db.releaseMemtableView(leasedView)
-				},
+			if hasMemSource && view != nil {
+				leasedView := view
+				view = nil
+				releaseView = false
+				return &leasedMergingIterator{
+					Iterator: it,
+					release: func() {
+						db.releaseMemtableView(leasedView)
+					},
+				}
 			}
-		}
 		if view != nil {
 			db.releaseMemtableView(view)
 			view = nil
@@ -15400,17 +15400,17 @@ func (db *DB) ReverseIterator(start, end []byte) (merging.Iterator, error) {
 		if iteratorDebugEnabled.Load() {
 			it = &debugIterator{Iterator: it, queueLen: queueLen, sourcesUsed: sourcesUsed}
 		}
-		if hasMemSource && view != nil {
-			leasedView := view
-			view = nil
-			releaseView = false
-			return &leasedMergingIterator{
-				Iterator: it,
-				release: func() {
-					db.releaseMemtableView(leasedView)
-				},
+			if hasMemSource && view != nil {
+				leasedView := view
+				view = nil
+				releaseView = false
+				return &leasedMergingIterator{
+					Iterator: it,
+					release: func() {
+						db.releaseMemtableView(leasedView)
+					},
+				}
 			}
-		}
 		if view != nil {
 			db.releaseMemtableView(view)
 			view = nil
