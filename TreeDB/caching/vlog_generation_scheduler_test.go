4b95601140f803090878acfc839e55ab051b32aa
Mikers <snissn@gmail.com>
2026-03-08 19:24:18 -1000

    tests: align maintenance gating expectations

--- TreeDB/caching/vlog_generation_scheduler_test.go
+++ TreeDB/caching/vlog_generation_scheduler_test.go
@@ -928,7 +928,7 @@ func TestVlogGenerationGC_SkipsDuringRecentForegroundWrites(t *testing.T) {
 
 	db.vlogGenerationLastGCUnixNano.Store(0)
 	db.lastForegroundWriteUnixNano.Store(time.Now().UnixNano())
-	db.maybeRunVlogGenerationMaintenance(true)
+	db.maybeRunVlogGenerationMaintenance(false)
 
 	dryRunCalls, realCalls, _ := recorder.recordedCalls()
 	if dryRunCalls != 0 || realCalls != 0 {
