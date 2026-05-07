8f34362ed1e84b6bdd3a2713a024fc22349a38d0
Mikers <snissn@gmail.com>
2026-05-07 10:20:48 -1000

    mongo gateway: avoid unused moreToCome find work

--- TreeDB/mongo_gateway/commands.go
+++ TreeDB/mongo_gateway/commands.go
@@ -1420,9 +1420,10 @@ func marshalCursorDocumentsMsgResponseWithID(requestID, responseTo int32, ns str
 }
 
 func appendWireInt32(dst []byte, v int32) []byte {
-	var buf [4]byte
-	binary.LittleEndian.PutUint32(buf[:], uint32(v))
-	return append(dst, buf[:]...)
+	n := len(dst)
+	dst = append(dst, 0, 0, 0, 0)
+	binary.LittleEndian.PutUint32(dst[n:], uint32(v))
+	return dst
 }
 
 func bsonArrayIndexKey(index int) string {
