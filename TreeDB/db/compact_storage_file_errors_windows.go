e188ea70614f882eb768f80a894edbe8d2327b0b
Mikers <snissn@gmail.com>
2026-05-09 15:58:43 -1000

    treedb: fix windows busy remove detection

--- TreeDB/db/compact_storage_file_errors_windows.go
+++ TreeDB/db/compact_storage_file_errors_windows.go
@@ -7,6 +7,11 @@ import (
 	"syscall"
 )
 
+const (
+	windowsErrorAccessDenied     syscall.Errno = 5
+	windowsErrorSharingViolation syscall.Errno = 32
+)
+
 func compactStorageIsBusyRemoveError(err error) bool {
-	return errors.Is(err, syscall.ERROR_SHARING_VIOLATION) || errors.Is(err, syscall.ERROR_ACCESS_DENIED)
+	return errors.Is(err, windowsErrorSharingViolation) || errors.Is(err, windowsErrorAccessDenied)
 }
