1c49eca6879f56fa465bb7cbc83744b5b9783e3e
Mikers <snissn@gmail.com>
2026-05-17 17:23:10 -1000

    experiments/colgranule: document graph dot preconditions

--- experiments/colgranule/vector_graph_dot_arm64.go
+++ experiments/colgranule/vector_graph_dot_arm64.go
@@ -4,6 +4,9 @@ package colgranule
 
 import axiomsimd "github.com/axiomhq/simd-go"
 
+// columnVectorGraphDotProductFloat32 returns the float32 dot product.
+// Precondition: left and right must have equal length; callers enforce graph
+// query and row-vector dimensionality before reaching this hot path.
 func columnVectorGraphDotProductFloat32(left, right []float32) float32 {
 	return axiomsimd.DotProductFloat32(left, right)
 }
