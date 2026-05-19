3f2bcd62a20ae5ab0691585c91045c1e1de04b32
Mikers <snissn@gmail.com>
2026-05-19 09:29:41 -1000

    collections: reject identity-only column manifests

--- TreeDB/collections/column_publish_write_path_test.go
+++ TreeDB/collections/column_publish_write_path_test.go
@@ -1249,6 +1249,7 @@ func TestColumnManifestRootDescriptorSystemDeltaReturnsPreparedUpdatedMetaM10B(t
 	if err != nil {
 		t.Fatalf("BuildColumnPublishPlan: %v", err)
 	}
+	identity = plan.UpdatedActiveManifest
 
 	rootName := collectionColumnManifestRootName("events")
 	iter, updatedMeta, err := col.buildRootDescriptorAndColumnManifestSystemDeltaIteratorAndMetaForMeta(
