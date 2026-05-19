b60ef000ccf17aba842f7c8f7c384fbc46670516
Mikers <snissn@gmail.com>
2026-05-19 10:56:15 -1000

    TreeDB column M13A validate retained manifest publish

--- TreeDB/collections/column_physical_asset.go
+++ TreeDB/collections/column_physical_asset.go
@@ -463,7 +463,7 @@ func (c *manifestCursor) bool() bool {
 
 func (c *manifestCursor) bytes() []byte {
 	value := c.bytesView()
-	if value == nil {
+	if c.err != nil {
 		return nil
 	}
 	return bytes.Clone(value)
