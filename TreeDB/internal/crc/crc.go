8155fbbd93f5f894e44b0d46fa625b7bdf1c6c78
Mikers <snissn@gmail.com>
2026-05-24 19:31:28 -1000

    Route TreeDB CRCs through internal facade

--- TreeDB/internal/crc/crc.go
+++ TreeDB/internal/crc/crc.go
@@ -1,13 +1,19 @@
+// Package crc is TreeDB's audited CRC-32 facade.
+//
+// All functions use the CRC-32/IEEE polynomial and preserve the byte-for-byte
+// values produced by github.com/snissn/go-crc32-asm.
 package crc
 
 import crc32 "github.com/snissn/go-crc32-asm"
 
 var table = crc32.MakeTable(crc32.IEEE)
 
+// Checksum returns the CRC-32/IEEE checksum of data.
 func Checksum(data []byte) uint32 {
 	return crc32.Checksum(data, table)
 }
 
+// ChecksumParts returns the CRC-32/IEEE checksum over the concatenation of parts.
 func ChecksumParts(parts ...[]byte) uint32 {
 	sum := uint32(0)
 	for _, p := range parts {
@@ -19,6 +25,7 @@ func ChecksumParts(parts ...[]byte) uint32 {
 	return sum
 }
 
+// Update extends sum with data using the CRC-32/IEEE polynomial.
 func Update(sum uint32, data []byte) uint32 {
 	if len(data) == 0 {
 		return sum
