f9bea689ee94b3a733a697e744896c601e5d8b58
Mikers <snissn@gmail.com>
2026-06-26 21:11:49 -1000

    fix: preserve Darwin column asset full sync

--- TreeDB/collections/column_asset_file_sync_darwin.go
+++ TreeDB/collections/column_asset_file_sync_darwin.go
@@ -2,30 +2,12 @@
 
 package collections
 
-import (
-	"os"
-	"runtime"
-
-	"golang.org/x/sys/unix"
-)
+import "os"
 
 func syncColumnAssetSegmentFile(file *os.File) error {
 	if file == nil {
 		return nil
 	}
-	fd := file.Fd()
-	for {
-		_, _, errno := unix.Syscall(unix.SYS_FDATASYNC, fd, 0, 0)
-		runtime.KeepAlive(file)
-		if errno == 0 {
-			return nil
-		}
-		if errno == unix.EINTR {
-			continue
-		}
-		if errno == unix.ENOSYS || errno == unix.EINVAL {
-			return file.Sync()
-		}
-		return errno
-	}
+	// Go uses F_FULLFSYNC for os.File.Sync on Darwin; keep that publish barrier.
+	return file.Sync()
 }
