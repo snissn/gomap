f9b01612a174ff186d1e7e51f18b883038e6f228
Mikers <snissn@gmail.com>
2026-07-13 03:42:59 -1000

    test(treedb): cover Windows closed file handles

---
+++ TreeDB/internal/commitlog/file_handle_closed_windows_test.go
@@ -0,0 +1,34 @@
+//go:build windows
+
+package commitlog
+
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"golang.org/x/sys/windows"
+)
+
+func TestFileHandleClosedForTestWindows(t *testing.T) {
+	file, err := os.CreateTemp(t.TempDir(), "closed-handle-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { _ = file.Close() })
+
+	_, statErr := file.Stat()
+	if fileHandleClosedForTest(statErr) {
+		t.Fatal("live file handle reported closed")
+	}
+	if err := file.Close(); err != nil {
+		t.Fatal(err)
+	}
+	_, statErr = file.Stat()
+	if !errors.Is(statErr, windows.ERROR_INVALID_HANDLE) {
+		t.Fatalf("closed file Stat error=%v want ERROR_INVALID_HANDLE", statErr)
+	}
+	if !fileHandleClosedForTest(statErr) {
+		t.Fatalf("closed file Stat error=%v not recognized as closed", statErr)
+	}
+}
