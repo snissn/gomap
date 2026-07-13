b2b787840f8a910d5557be6778bdc44d2e13cabd
Mikers <snissn@gmail.com>
2026-07-13 08:40:54 -1000

    test(treedb): preserve conditional close admission witness

--- TreeDB/db/conditional_txn.go
+++ TreeDB/db/conditional_txn.go
@@ -443,9 +443,6 @@ func (tx *ConditionalTxn) validateReadSetOnly() error {
 	}
 	tx.db.writeMu.RLock()
 	defer tx.db.writeMu.RUnlock()
-	if err := tx.db.checkWriteAdmissionLocked(); err != nil {
-		return err
-	}
 	tx.db.commitMu.Lock()
 	err := tx.validateReadSetAtPublish()
 	tx.db.commitMu.Unlock()
