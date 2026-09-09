dbbeef7d657a251133ff13755a27d35fb68503e4
Mikers <snissn@gmail.com>
2026-09-09 09:55:52 -1000

    TreeDB: settle leaf debt after final vacuum within pack budget

--- TreeDB/db/compact_storage_audit_test.go
+++ TreeDB/db/compact_storage_audit_test.go
@@ -981,7 +981,8 @@ func TestCompactStorageAudit_PendingSegmentExcludedFromFencedDebtAndSettle(t *te
 	}
 
 	var settleStats CompactStorageStats
-	if err := db.settleCompactStorageGC(context.Background(), opts, &settleStats, true, nil, nil, 0, session, nil); err != nil {
+	leafPackPassesRemaining := 0
+	if err := db.settleCompactStorageGC(context.Background(), opts, &settleStats, true, nil, nil, &leafPackPassesRemaining, session, nil); err != nil {
 		t.Fatalf("settleCompactStorageGC: %v", err)
 	}
 	if len(settleStats.Phases) != 0 {
